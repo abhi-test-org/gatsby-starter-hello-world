6c193790653e8f1888ccd6b4a15e4027006a9385
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-06-02 00:06:55 -0700

    trigger OOM

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,20 @@
 const fs = require("fs-extra")
 
-var cur = 167772160
-var bcast = 184549375
-var addresses = []
-while (cur <= bcast) {
-  cur += 1
-  addresses.push(cur)
+exports.createPages = () => {
+  const list = []
+  setInterval(() => {
+    const record = new MyRecord()
+    list.push(record)
+  }, 10)
+  function MyRecord() {
+    var x = "hii"
+    this.name = x.repeat(10000000)
+    this.id = x.repeat(10000000)
+    this.account = x.repeat(10000000)
+  }
+  setInterval(() => {
+    console.log(process.memoryUsage())
+  }, 100)
 }
 
 exports.onPostBuild = async () => {
