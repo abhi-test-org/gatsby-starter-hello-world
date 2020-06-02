8fd45370b0db5702e4df6c5db0c3e359a29742f3
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-06-02 15:01:17 -0700

    fix log

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,29 @@
 const fs = require("fs-extra")
 // const faker = require("faker")
 
-// exports.createPages = () => {
-//   let list = []
-//   setInterval(() => {
-//     const record = new MyRecord()
-//     list = [...list, record]
-//   }, 1)
-//   function MyRecord() {
-//     var x = faker.name.findName()
-//     this.name = x.repeat(20000000)
-//     this.id = x.repeat(20000000)
-//     this.account = x.repeat(20000000)
-//     this.account2 = x.repeat(20000000)
-//     this.account4 = x.repeat(20000000)
-//   }
-//   setInterval(() => {
-//     console.log(process.memoryUsage())
-//   }, 100)
+exports.createPages = () => {
+  let list = []
+  let list2 = []
+  setInterval(() => {
+    const record = new MyRecord()
+    list = [...list, record]
+  }, 1)
+
+  setInterval(() => {
+    const record = new MyRecord()
+    list2 = [...list2, record]
+  }, 10)
+  function MyRecord() {
+    var x = 
+    this.name = faker.name.findName().repeat(20000000)
+    this.id = faker.random.uuid().repeat(20000000)
+    this.id2 = faker.random.uuid().repeat(20000000)
+    this.id3 = faker.random.uuid().repeat(20000000)
+    this.id4 = faker.random.uuid().repeat(20000000)
+  }
+  setInterval(() => {
+    console.log(process.memoryUsage())
+  }, 100)
 // }
 
 exports.onPostBuild = async () => {
