0396fa4bb9ef8430d21bfbabc72db190243965be
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-06-02 13:54:41 -0700

    remove oom

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,24 @@
 const fs = require("fs-extra")
-const faker = require("faker")
+// const faker = require("faker")
 
-exports.createPages = () => {
-  let list = []
-  setInterval(() => {
-    const record = new MyRecord()
-    list = [...list, record]
-  }, 1)
-  function MyRecord() {
-    var x = faker.name.findName()
-    this.name = x.repeat(20000000)
-    this.id = x.repeat(20000000)
-    this.account = x.repeat(20000000)
-    this.account2 = x.repeat(20000000)
-    this.account4 = x.repeat(20000000)
-  }
-  setInterval(() => {
-    console.log(process.memoryUsage())
-  }, 100)
-}
+// exports.createPages = () => {
+//   let list = []
+//   setInterval(() => {
+//     const record = new MyRecord()
+//     list = [...list, record]
+//   }, 1)
+//   function MyRecord() {
+//     var x = faker.name.findName()
+//     this.name = x.repeat(20000000)
+//     this.id = x.repeat(20000000)
+//     this.account = x.repeat(20000000)
+//     this.account2 = x.repeat(20000000)
+//     this.account4 = x.repeat(20000000)
+//   }
+//   setInterval(() => {
+//     console.log(process.memoryUsage())
+//   }, 100)
+// }
 
 exports.onPostBuild = async () => {
   await fs.copy("./functions", "./public/functions")
