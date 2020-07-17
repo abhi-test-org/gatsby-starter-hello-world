cc0f2d929662263524c044a3871b4c7b93b6ae13
Abhi Aiyer <abhiaiyer91@gmail.com>
2020-07-17 12:29:20 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ const Image = () => (
         placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
-              ...GatsbyImageSharpFlui
+              ...GatsbyImageSharpFluid
             }
           }
         }
