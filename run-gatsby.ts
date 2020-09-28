1d96205eb3d2dad942f5a07afda63d216fd89eeb
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-09-28 11:52:35 -0700

    ipc plugin test

---
+++ run-gatsby.ts
@@ -0,0 +1,38 @@
+import * as path from "path"
+import { spawn } from "child_process"
+
+function run() {
+  const proc = spawn(path.join(__dirname, `runner-command`), [], {
+    // settings for streams are as follows: [stdin, stdout, stderr(, ipc)]
+    stdio: [`pipe`, `pipe`, `pipe`, `ipc`],
+    detached: true,
+    cwd: process.cwd(),
+
+    env: {
+      ...process.env,
+    },
+  })
+
+  proc.stderr.on(`data`, buf => {
+    let log = buf.toString()
+
+    console.log(`ERROR`, log)
+  })
+  proc.stdout.on(`data`, buf => {
+    let log = buf.toString()
+
+    console.log(`LOG`, log)
+  })
+
+  proc.on("message", data => {
+    console.log(`STRUCTURED DATA`, data)
+  })
+
+  proc.on(`exit`, data => {
+    console.log(data)
+    proc.kill()
+    // process.kill(proc.pid)
+  })
+}
+
+run()
