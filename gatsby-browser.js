86debaf3080df678e24d1b76fe44358ab60a5798
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-11-01 19:23:06 -0800

    foo

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,8 @@ const io = require("socket.io-client")
 function PreviewProvider({ children }) {
   const [showReloadBar, setShowReloadBar] = React.useState(false)
 
+  const [showInProgressBar, setInProgess] = React.useState(false)
+
   React.useEffect(() => {
     const socket = io()
 
@@ -11,12 +13,20 @@ function PreviewProvider({ children }) {
       console.log("Connected")
     })
 
-    socket.on("reload", () => {
+    socket.on("reload", data => {
+      console.log(data)
+      setInProgess(false)
       setShowReloadBar(true)
 
       setTimeout(() => {
         window.location.reload()
       }, 1000)
+    })
+
+    socket.on("building", data => {
+      console.log(data)
+      setInProgess(true)
+      setShowReloadBar(false)
     })
 
     return () => {
@@ -39,6 +49,22 @@ function PreviewProvider({ children }) {
           }}
         >
           Your Preview is reloading...
+        </div>
+      )}
+
+      {showInProgressBar && (
+        <div
+          style={{
+            position: `fixed`,
+            width: `100%`,
+            top: 0,
+            left: 0,
+            backgroundColor: `purple`,
+            color: `white`,
+            textAlign: `center`,
+          }}
+        >
+          Your Preview is rebuilding...
         </div>
       )}
 
